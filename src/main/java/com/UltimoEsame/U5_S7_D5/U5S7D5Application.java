a20828a60f75480550f0431f6de4743d8208ce6e
AdeleGropplero <adele.gdt@hotmail.com>
2025-02-21 16:34:07 +0100

    first

---
+++ src/main/java/com/UltimoEsame/U5_S7_D5/U5S7D5Application.java
@@ -0,0 +1,13 @@
+package com.UltimoEsame.U5_S7_D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5S7D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5S7D5Application.class, args);
+	}
+
+}
